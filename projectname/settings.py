9a98f33e6a93941a9b62a05db7fcd3ac54c97d6b
fooggreitan <111742480+fooggreitan@users.noreply.github.com>
2024-02-11 18:34:17 +0300

    save #2

--- projectname/settings.py
+++ projectname/settings.py
@@ -74,19 +74,19 @@ WSGI_APPLICATION = "projectname.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "qualifier",
-        'USER': 'postgres',
-        'PASSWORD': 'Vvhfddrvgd123+',
-        'HOST': 'localhost',
-        'PORT': 5432
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "qualifier",
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Vvhfddrvgd123+',
+    #     'HOST': 'localhost',
+    #     'PORT': 5432
+    # }
 
     # 'club_db': {
     #     'NAME': 'qualifier',
